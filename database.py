3c8ffd67e909a959e8a348ea2b78165c6a10c800
Hrishikesh <hrishikeshshendage313@gmail.com>
2026-02-05 12:29:03 +0530

    minor changes for deployment

--- database.py
+++ database.py
@@ -5,14 +5,8 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-USER = os.getenv("user")
-PASSWORD = os.getenv("password")
-HOST = os.getenv("host")
-PORT = os.getenv("port")
-DBNAME = os.getenv("dbname")
-
 # Construct the SQLAlchemy connection string
-DATABASE_URL = f"postgresql+psycopg2://{USER}:{PASSWORD}@{HOST}:{PORT}/{DBNAME}?sslmode=require"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 
 # SQLALCHEMY_DATABASE_URL = os.environ.get("DATABASE_URL")
